da7c1f07ec2445909282155c169213f80c758b1b
jonathon.evans <jonathon.evans@students.plymouth.ac.uk>
2019-12-28 13:23:03 +0000

    database created

--- BattleHost/app.js
+++ BattleHost/app.js
@@ -1,3 +1,11 @@
 'use strict';
+var MongoClient = require('mongodb').MongoClient;
+var dbUrl = "mongodb://localhost:27017/battleHost";
 
 console.log('Hello world');
+
+MongoClient.connect(dbUrl, function (err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
