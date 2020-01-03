b09eedf4fb1ca87c702bd87ec6db43cc2e6aa583
jonathon.evans <jonathon.evans@students.plymouth.ac.uk>
2020-01-03 20:04:55 +0000

    server created

--- BattleHost/app.js
+++ BattleHost/app.js
@@ -1,11 +1,34 @@
 'use strict';
-var MongoClient = require('mongodb').MongoClient;
-var dbUrl = "mongodb://localhost:27017/battleHost";
+var MongoClient = require("mongodb").MongoClient;
+var dbUrl = "mongodb://localhost:27017/";
 
-console.log('Hello world');
+var http = require("http");
+var port = 9000;
 
-MongoClient.connect(dbUrl, function (err, db) {
+console.log('Hello world ------------------------------');
+
+
+
+//Server operations
+var server = http.createServer(function (request, response) {
+    
+});
+
+server.listen(port, function () {
+    console.log("listening -----------------------------");
+});
+
+//Database operations
+MongoClient.connect(dbUrl, { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
-    console.log("Database created!");
-    db.close();
+    console.log("Database created! ----------------------------");
+
+    var dbo = db.db("battleHostDB");
+
+    dbo.createCollection("connections", function (err, res) {
+        if (err) throw err;
+        console.log("Collection created. ----------------------------");
+
+        db.close();
+    });
 });
